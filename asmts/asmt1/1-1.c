d6981dfb423d3a226beb146e487c314f9c4f50cc
recuxer <ooowea@gmail.com>
2018-01-12 14:04:38 -0800

    added file header

--- asmts/asmt1/1-1.c
+++ asmts/asmt1/1-1.c
@@ -1,35 +1,42 @@
+/*
+	Author: John Nemeth
+	Description: printing byte representations of memory addresses
+	Sources: Class/assignment material
+*/
+
 #include <stdio.h>
 
 void printBytes(unsigned char *start, int len) {
- for (int i = 0; i < len; ++i) {
- printf(" %.2x", start[i]);
- }
- printf("\n");
+	for (int i = 0; i < len; ++i) {
+	 printf(" %.2x", start[i]);
+	}
+	printf("\n");
 }
 
 void printInt(int x) {
- printBytes((unsigned char *) &x, sizeof(int));
+	printBytes((unsigned char *) &x, sizeof(int));
 }
 
 void printFloat(float x) {
- printBytes((unsigned char *) &x, sizeof(float));
+	printBytes((unsigned char *) &x, sizeof(float));
 }
 
-printShort() {
-
-}
-
-printLong() {
+void printShort() {
 
 }
 
-printDouble() {
+void printLong() {
+
+}
+
+void printDouble() {
 
 }
 
 
 int main () {
+	unsigned int x = 254;
+	printInt(x);
 
-
-return 0;
+	return 0;
 }
