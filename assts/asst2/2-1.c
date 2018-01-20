ccb44f7bcbae78254eac41c6ea7b067f67f41d3d
recuxer <ooowea@gmail.com>
2018-01-19 19:09:26 -0800

    2-1, 2-2 complete

--- assts/asst2/2-1.c
+++ assts/asst2/2-1.c
@@ -8,24 +8,17 @@
 
 // creates mask based on passed int
 int mask(int n) {
-	/*
-	unsigned int x = 0x7FFFFFFF;
-	x = x >> (31 - n);
-	return x;
-	*/
 	
-	// x is 1111...1110 in binary
+	// x is 1111...1110 in binary *note: could've used signed int -1 << 1*
 	unsigned int x = 0xFFFFFFFE;
 	// since shifting by 32 is undefined, we ensure we don't shift by 32
 	x = x << (n - 1);
-	// should find a better way to accomplish because it doesn't satisfy mask(0) correctly
 	return ~x;
 }
 
 // main function with examples
 int main () {
 
-	printf("mask int: 0 - result: 0x%X\n", mask(0));
 	printf("mask int: 1 - result: 0x%X\n", mask(1));
 	printf("mask int: 2 - result: 0x%X\n", mask(2));
 	printf("mask int: 3 - result: 0x%X\n", mask(3));
--- assts/asst2/2-2.c
+++ assts/asst2/2-2.c
@@ -1,20 +1,39 @@
 /*
 	Author: John Nemeth
-	Description: return signed byte i of signed x
+	Description: return signed byte i of x
 	Sources: Class/assignment material
 */
 
 #include <stdio.h>
 
+// extract signed byte from unsigned int x
 unsigned int extract(unsigned int x, int i) {
-	
-	return x;
+	// create mask
+	unsigned int mask = 0xFF;
+	// transform into required bits for shifting mask
+	i = i << 3;
+	// isolate byte
+	x = x & (mask << i);
+	// move byte to correct position
+	x = x >> i;
+	//cast to signed char for correct output
+	return (signed char) x;
 }
 
 // main function with examples
 int main () {
-
-	printf("\tpassed x: 0x12345678 - byte pos: 0\n\texpected: 0x00000078 - returned: 0x%08X\n", extract(0x12345678, 0));
+	// 78 == b01111000, 0 furthest left
+	printf("\tpassed x: 0x12345678 - byte pos: 0\n");
+	printf("\texpected: 0x00000078 - returned: 0x%08X\n---\n", extract(0x12345678, 0));
+	// EF == b11101111, 1 furthest left
+	printf("\tpassed x: 0xABCDEF00 - byte pos: 1\n");
+	printf("\texpected: 0xFFFFFFEF - returned: 0x%08X\n---\n", extract(0xABCDEF00, 1));
+	// CD == b11001101, 1 furthest left
+	printf("\tpassed x: 0xABCDEF00 - byte pos: 2\n");
+	printf("\texpected: 0xFFFFFFCD - returned: 0x%08X\n---\n", extract(0xABCDEF00, 2));
+	// AB == b10101011, 1 furthest left
+	printf("\tpassed x: 0xABCDEF00 - byte pos: 3\n");
+	printf("\texpected: 0xFFFFFFAB - returned: 0x%08X\n---\n", extract(0xABCDEF00, 3));
 		
 	return 0;
 }
