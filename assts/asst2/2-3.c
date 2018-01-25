60a0f3eb9ac28bf209a1b8e4b3501f708e86128b
recuxer <ooowea@gmail.com>
2018-01-24 20:31:45 -0800

    fixed logic

--- assts/asst2/2-3.c
+++ assts/asst2/2-3.c
@@ -15,10 +15,11 @@ int ge(float x, float y) {
 	ux <<= 1;	//drop sign bit of ux
 	uy <<= 1;	//drop sign bit of uy
 
-	// case1: equal in value then signs can be whatever
-	// case2: if x greater, then sign can be equal or less than y (x can't be negative if y positive)
-	// case3: if x smaller, then sign must be less than y (needs to be positive)
-	return ((ux == uy) && ((sx >= sy) || (sx < sy))) || ((ux > uy) && (sx <= sy)) || ((ux < uy) && (sx < sy));
+	// case1: if ux and uy both equal to zero, then sign not checked
+	// case2: if ux and uy equal and not equal to zero(case already checked), then xsign must be less than ysign
+	// case3: if ux greater, then sign can be equal or less than y (x can't be negative if y positive)
+	// case4: if ux smaller, then sign must be less than y (needs to be positive)
+	return ((ux == 0) && (uy == 0)) || ((ux == uy) && (sx < sy)) || ((ux > uy) && (sx <= sy)) || ((ux < uy) && (sx < sy));
 
 }
 
@@ -30,7 +31,8 @@ int main () {
 	printf("\tx: 0.0f ### y: 1.0f ###   result: %x\n", ge(0.0f, 1.0f));
 	printf("\tx: 1.0f ### y: 0.0f ###   result: %x\n", ge(1.0f, 0.0f));
 	printf("\tx: 0.0f ### y: -1.0f ###  result: %x\n", ge(0.0f, -1.0f));
-	printf("\tx: 5.5f ### y: -6.5f ###  result: %x\n", ge(5.5f, -6.5f));
+	printf("\tx: 5.5f ### y: -5.5f ###  result: %x\n", ge(5.5f, -5.5f));
+	printf("\tx: -5.5f ### y: 5.5f ###  result: %x\n", ge(-5.5f, 5.5f));
 		
 	return 0;
 }
